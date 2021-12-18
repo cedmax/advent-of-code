870409a0ef464240934399db606b9945f8d7c3f7
Marco Cedaro <cedmax@users.noreply.github.com>
2021-12-18 13:39:32 +0000

    2020/25

--- 2020/25/index.js
+++ 2020/25/index.js
@@ -1,4 +1,22 @@
-const input = require("../../utils/getInput")(__dirname, { split: null });
+const input = require("../../utils/getInput")(__dirname, { split: "\n" }).map(
+  Number
+);
 
-console.log(input);
-console.log("METHOD NOT IMPLEMENTED");
+function expMod(base, exp, mod) {
+  if (exp == 0) return 1;
+  if (exp % 2 == 0) {
+    return Math.pow(expMod(base, exp / 2, mod), 2) % mod;
+  } else {
+    return (base * expMod(base, exp - 1, mod)) % mod;
+  }
+}
+const [doorPublicKey, keyPublicKey] = input;
+
+let v = 1;
+let keyLoopSize = 0;
+while (v !== keyPublicKey) {
+  v = (v * 7) % 20201227;
+  keyLoopSize++;
+}
+
+console.log(expMod(doorPublicKey, keyLoopSize, 20201227));
