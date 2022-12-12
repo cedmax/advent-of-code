b3cacdff47d8be333a2921d92655b82423be4537
Marco Cedaro <cedmax@users.noreply.github.com>
2022-12-12 10:09:16 +0000

    oops

--- utils/open.js
+++ utils/open.js
@@ -2,6 +2,6 @@ const open = require("open");
 
 const date = new Date();
 const year = date.getFullYear();
-const day = date.getMonth();
+const day = date.getDate();
 
 open(`https://adventofcode.com/${year}/day/${day}`);
