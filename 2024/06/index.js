7969ad605fea5f73d41239bb85714c933ae42108
Marco Cedaro <cedmax@users.noreply.github.com>
2024-12-07 18:57:33 +0000

    found the time to do day 6

--- 2024/06/index.js
+++ 2024/06/index.js
@@ -56,79 +56,45 @@ const getNextAction = ([x, y], direction) => {
 
 const firstPart = (map) => {
   let direction = "^";
-  const path = [startCoords];
+  const path = [{ pos: startCoords, direction }];
 
-  while (isInBounds(path[path.length - 1])) {
-    const [x, y] = path[path.length - 1];
+  while (isInBounds(path[path.length - 1].pos)) {
+    const [x, y] = path[path.length - 1].pos;
 
     const { next, rotate } = getNextAction([x, y], direction);
 
     if (map[next[1]]?.[next[0]] === "#") {
       direction = rotate;
     } else {
-      path.push(next);
+      // added for part2, if we are in the same position as before and
+      // about to go in the same direction, then it's a loop
+      if (path.length > 1 && path.find(({ pos: [stepX, stepY], direction: stepDirection }) => stepX === next[0] && stepY === next[1] && stepDirection === direction)) {
+        return [];
+      }
+      path.push({ pos: next, direction });
     }
   }
-  const unique = new Set(path.map((a) => JSON.stringify(a)));
-  return unique.size - 1;
+
+  path.pop();
+  return Array.from(new Set(path.map((a) => JSON.stringify(a.pos)))).map((a) => JSON.parse(a));
 };
 
-console.log(firstPart(input));
+const path = firstPart(input);
+console.log(path.length);
 
-// I gave up: brute forcing takes ages to run and it's impossible to debug
-// and I have no time to think about a more clever approach
+// god this is slow
+let loops = 0;
+for (const step of path) {
+  // skipping the first block as per the rules
+  if (JSON.stringify(step) === JSON.stringify(startCoords)) {
+    continue;
+  }
+  const [x, y] = step;
+  const map = JSON.parse(JSON.stringify(input));
+  map[y][x] = "#";
+  if (firstPart(map).length === 0) {
+    loops++;
+  }
+}
 
-// const checkLoop = (pathSoFar, direction, map) => {
-//   const path = [...pathSoFar];
-//   while (isInBounds(path[path.length - 1].next)) {
-//     const {
-//       next: [x, y]
-//     } = path[path.length - 1];
-
-//     const { next, rotate } = getNextAction([x, y], direction);
-
-//     if (map[next[1]]?.[next[0]] === "#") {
-//       direction = rotate;
-//     } else {
-//       if (path.find((a) => JSON.stringify(a.next) === JSON.stringify(next) && a.direction === direction)) {
-//         return true;
-//       }
-//       path.push({ next, direction });
-//     }
-//   }
-//   return false;
-// };
-
-// const secondPart = (matrix) => {
-//   let direction = "^";
-//   const newBlocks = [];
-//   const path = [{ next: startCoords, direction: "^" }];
-//   while (isInBounds(path[path.length - 1].next)) {
-//     const [x, y] = path[path.length - 1].next;
-
-//     const { next, rotate } = getNextAction([x, y], direction);
-
-//     if (input[next[1]]?.[next[0]] === "#") {
-//       direction = rotate;
-//     } else {
-//       path.push({ next, direction });
-//     }
-
-//     const { next: block } = getNextAction(next, direction);
-//     const clonedInput = JSON.parse(JSON.stringify(matrix));
-//     if (clonedInput[block[1]]?.[block[0]]) {
-//       clonedInput[block[1]][block[0]] = "#";
-//       if (checkLoop(path, direction, clonedInput)) {
-//         newBlocks.push(block);
-//         console.log(newBlocks);
-//       }
-//     }
-//   }
-
-//   const containsStart = !!newBlocks.find((a) => (a[0] === startCoords[0]) & (a[1] === startCoords[1]));
-
-//   const unique = new Set(newBlocks.map((a) => JSON.stringify(a)));
-//   return unique.size - (containsStart ? 1 : 0);
-// };
-
-// console.log(secondPart(input));
+console.log(loops);
