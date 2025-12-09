0ec97bf601e202da5eff141831fe4911686f01bc
Marco Cedaro <cedmax@users.noreply.github.com>
2025-12-09 17:29:08 +0000

    more old stuff

--- 2015/19/index.js
+++ 2015/19/index.js
@@ -1,4 +1,22 @@
-const input = require("../../utils/getInput")(__dirname, { split: null });
+const input = require("../../utils/getInput")(__dirname, { split: "\n\n" });
 
-console.log(input);
-console.log("METHOD NOT IMPLEMENTED");
+const [rawParts, text] = input;
+const parts = rawParts.split("\n").reduce((map, ln) => {
+  const [part, replacement] = ln.split(" => ");
+  return [...map, [part, replacement]];
+}, []);
+
+const results = new Set();
+for (let [part, replacement] of parts) {
+  const re = new RegExp(`(${part})`, "g");
+  const matches = [...text.matchAll(re)];
+  matches.forEach(({ index }) => {
+    results.add(
+      text.substring(0, index) +
+        replacement +
+        text.substring(index + part.length)
+    );
+  });
+}
+
+console.log(results.size);
--- 2016/14/index.js
+++ 2016/14/index.js
@@ -1,4 +1,27 @@
+const { createHash } = require("crypto");
 const input = require("../../utils/getInput")(__dirname, { split: null });
 
-console.log(input);
-console.log("METHOD NOT IMPLEMENTED");
+const hasher = (str) => createHash("md5").update(`${input}${str}`).digest("hex");
+
+let keysFound = 0;
+let currentIndex = 0;
+let lastIndex = 0;
+while (keysFound < 64) {
+  const hash = hasher(currentIndex);
+  const re = /(.)\1\1/;
+  const match = hash.match(re);
+  if (match) {
+    const re2 = new RegExp(`(${match[1]})\\1\\1\\1\\1`);
+    for (let i = currentIndex + 1; i < currentIndex + 1000; i++) {
+      const newhash = hasher(i);
+      if (newhash.match(re2)) {
+        lastIndex = currentIndex;
+        keysFound++;
+        break;
+      }
+    }
+  }
+  currentIndex++;
+}
+
+console.log(lastIndex);
