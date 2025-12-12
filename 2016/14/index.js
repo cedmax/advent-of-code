59e559490b651556aca2a27db6a4cab0ebcfe387
cedmax <cedmax@users.noreply.github.com>
2025-12-12 08:41:15 +0000

    optimised 2016/14

--- 2016/14/index.js
+++ 2016/14/index.js
@@ -1,27 +1,32 @@
 const { createHash } = require("crypto");
 const input = require("../../utils/getInput")(__dirname, { split: null });
 
-const hasher = (str) => createHash("md5").update(`${input}${str}`).digest("hex");
+const hashCache = new Map();
+const hasher = (index) => {
+  if (!hashCache.has(index)) {
+    hashCache.set(index, createHash("md5").update(`${input}${index}`).digest("hex"));
+  }
+  return hashCache.get(index);
+};
 
 let keysFound = 0;
 let currentIndex = 0;
-let lastIndex = 0;
-while (keysFound < 64) {
+
+while (true) {
   const hash = hasher(currentIndex);
   const re = /(.)\1\1/;
   const match = hash.match(re);
   if (match) {
-    const re2 = new RegExp(`(${match[1]})\\1\\1\\1\\1`);
     for (let i = currentIndex + 1; i < currentIndex + 1000; i++) {
       const newhash = hasher(i);
-      if (newhash.match(re2)) {
-        lastIndex = currentIndex;
+      if (newhash.includes(match[1].repeat(5))) {
         keysFound++;
         break;
       }
     }
   }
+  if (keysFound === 64) break;
   currentIndex++;
 }
 
-console.log(lastIndex);
+console.log(currentIndex);
